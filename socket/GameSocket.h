dcaea108b3100fb7cf04773cbe0f1153db4c9895
zhupengcheng <z5130761@gmail.com>
2017-12-28 18:34:06 +0800

    Add socket file

---
+++ socket/GameSocket.h
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////
+/// Copyright(c) 2017-2099, zhu, All Rights Reserved
+/// Author：  zhu peng cheng
+/// Created： 2017/12/28
+/// Describe：
+/////////////////////////////////////////////////////////////////////////
+
+#ifndef _GAME_SOCKET_H
+#define _GAME_SOCKET_H
+
+namespace GameSocketLib
+{
+    class BaseSocket
+    {
+
+    }
+}
+
+#endif // _GAME_SOCKET_H
