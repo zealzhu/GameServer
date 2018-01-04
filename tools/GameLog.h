3489a0229168413b1fd16a564befe2779f68f2c1
zhupengcheng <z5130761@gmail.com>
2018-01-04 21:11:53 +0800

    Fix log param

--- tools/GameLog.h
+++ tools/GameLog.h
@@ -11,12 +11,13 @@
 #include <spdlog/spdlog.h>
 
 // {文件}::{函数}::{行}#{输出信息}
-#define logger_trace(fmt, ...) GameTools::logger->GetLogger()->trace("{}::{}()#{}: " fmt, __FILE__ , __FUNCTION__, __LINE__, __VA_ARGS__)
-#define logger_debug(fmt, ...) GameTools::logger->GetLogger()->debug("{}::{}()#{}: " fmt, __FILE__ , __FUNCTION__, __LINE__, __VA_ARGS__)
-#define logger_info(fmt, ...) GameTools::logger->GetLogger()->info("{}::{}()#{}: " fmt, __FILE__ , __FUNCTION__, __LINE__, __VA_ARGS__)
-#define logger_warn(fmt, ...) GameTools::logger->GetLogger()->warn("{}::{}()#{}: " fmt, __FILE__ , __FUNCTION__, __LINE__, __VA_ARGS__)
-#define logger_error(fmt, ...) GameTools::logger->GetLogger()->error("{}::{}()#{}: " fmt, __FILE__ , __FUNCTION__, __LINE__, __VA_ARGS__)
-#define logger_critical(fmt, ...) GameTools::logger->GetLogger()->critical("{}::{}()#{}: " fmt, __FILE__ , __FUNCTION__, __LINE__, __VA_ARGS__)
+// ## 用来去除空参数时前面的逗号
+#define logger_trace(fmt, ...) GameTools::logger->GetLogger()->trace("{}::{}()#{}: " fmt, __FILE__ , __FUNCTION__, __LINE__, ## __VA_ARGS__)
+#define logger_debug(fmt, ...) GameTools::logger->GetLogger()->debug("{}::{}()#{}: " fmt, __FILE__ , __FUNCTION__, __LINE__, ## __VA_ARGS__)
+#define logger_info(fmt, ...) GameTools::logger->GetLogger()->info("{}::{}()#{}: " fmt, __FILE__ , __FUNCTION__, __LINE__, ## __VA_ARGS__)
+#define logger_warn(fmt, ...) GameTools::logger->GetLogger()->warn("{}::{}()#{}: " fmt, __FILE__ , __FUNCTION__, __LINE__, ## __VA_ARGS__)
+#define logger_error(fmt, ...) GameTools::logger->GetLogger()->error("{}::{}()#{}: " fmt, __FILE__ , __FUNCTION__, __LINE__, ## __VA_ARGS__)
+#define logger_critical(fmt, ...) GameTools::logger->GetLogger()->critical("{}::{}()#{}: " fmt, __FILE__ , __FUNCTION__, __LINE__, ## __VA_ARGS__)
 
 namespace GameTools
 {
